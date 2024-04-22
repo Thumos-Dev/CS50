6e63f2569ed7ee1886179275edc302308d39cbe9
Thumos <cam.s.lewis@gmail.com>
2024-04-22 18:36:15 -0400

    Committing the solution for the mario-more problem in Week 1

--- Week_1-C/mario-more/mario.c
+++ Week_1-C/mario-more/mario.c
@@ -1,17 +1,43 @@
 #include <cs50.h>
 #include <stdio.h>
 
+int getHeight();
+void printPyramid(int height);
+
 int main(void)
 {
-    // Prompts the user for the height of the block (#) pyramid
-    int height;
+    int height = getHeight();
+    printPyramid(height);
+}
 
+int getHeight()
+{
+    int height;
     do
     {
         height = get_int("Height: ");
     }
     while (height < 1 || height > 8);
+    return height;
+}
 
-    printf("Height is %i\n", height);
-
+void printPyramid(int height)
+{
+    for (int row = 0; row < height; row++)
+    {
+        for (int spaces = row + 1; spaces < height; spaces++)
+        {
+            printf(" "); // Print spaces for alignment
+        }
+        for (int bricks = height + row + 1; bricks > height; bricks--)
+        {
+            printf("#"); // Print '#' characters for the left "bricks"
+        }
+        printf("  "); // Prints double spaces in between the "bricks"
+        for (int bricks = height + row + 1; bricks > height; bricks--)
+        {
+            printf("#"); // Print '#' characters for the  right "bricks"
+        }
+        printf("\n");
+    }
 }
